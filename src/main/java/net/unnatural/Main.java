00cff7ab499d6ae1f684c5264b8ddb1babe2032f
unnatural <karos2646@gmail.com>
2023-06-12 00:53:06 +0330

    Update Main.java

--- src/main/java/net/unnatural/Main.java
+++ src/main/java/net/unnatural/Main.java
@@ -1,6 +1,7 @@
 package net.unnatural;
 
 import org.jetbrains.annotations.NotNull;
+import com.moandjiezana.toml.Toml;
 
 import javax.tools.Tool;
 import javax.tools.ToolProvider;
@@ -10,13 +11,17 @@ import java.io.FileOutputStream;
 import java.util.Objects;
 
 public class Main {
+    //\/default value\/
+    public static String GroupID = "./build/net/test";
     static FileOutputStream fos;
+    static File tomlfile = new File("./config.toml");
+    static Toml toml = new Toml().read(tomlfile);
 
     static {
         try {
-            fos = new FileOutputStream("./build/net/test");
+            fos = new FileOutputStream(GetToml());
         } catch (FileNotFoundException e) {
-            System.out.println("CANNOT FIND BUILD/NET/TEST");
+            System.out.println("AN ERROR OCCURRED WHILE TRYING TO READ TOML FILE");
             System.out.println("CAUSE: " + e.getCause());
             System.out.println("STACK TRACE: ");
             e.printStackTrace();
@@ -26,6 +31,8 @@ public class Main {
     static Tool javac = ToolProvider.getSystemJavaCompiler();
 
     public static void main(String @NotNull [] args)  {
+        System.out.println("Configuring toml");
+        GetToml();
         System.out.println("Getting files");
         String[] files = getFiles();
         System.out.println("Compiling");
@@ -37,9 +44,14 @@ public class Main {
     }
 
     public static String[] getFiles() {
-            //Creating a File object for directory
-            File directoryPath = new File("./src/net/test");
-            //List of all files and directories
-            return directoryPath.list();
+        //Creating a File object for directory
+        File directoryPath = new File(GetToml());
+        //List of all files and directories
+        return directoryPath.list();
+    }
+
+    public static String GetToml() {
+        GroupID = toml.getString("GroupID");
+        return GroupID;
     }
 }
