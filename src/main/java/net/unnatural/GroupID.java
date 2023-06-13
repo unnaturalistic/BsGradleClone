d0fef07f97fcff070f91b9539abf6424482a0b60
unnatural <karos2646@gmail.com>
2023-06-13 23:02:35 +0330

    Added tasks arraylist

--- src/main/java/net/unnatural/GroupID.java
+++ src/main/java/net/unnatural/GroupID.java
@@ -1,8 +1,11 @@
 package net.unnatural;
 
+import java.util.ArrayList;
+
 class GroupID {
     public static String net;
     public static String name;
     public static String projectname;
     public static String mainclass;
+    public static ArrayList<String> Tasks = new ArrayList<String>();
 }
