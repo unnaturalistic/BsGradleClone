433b6adbb1a473c5b40f5beb19e6fa3731a5a7ce
unnatural <93143883+unnaturalistic@users.noreply.github.com>
2023-06-22 23:57:25 +0330

    Update Config.java

--- src/main/java/net/unnatural/Config.java
+++ src/main/java/net/unnatural/Config.java
@@ -1,5 +1,8 @@
 package net.unnatural;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Config {
     public static String net = "net";
     public static String name = "example";
@@ -7,21 +10,27 @@ public class Config {
     public static String projectnameingroupid;
     public static String MainClass = "Main";
     public static String MainClassfile;
+    static Map<String, String> map = new HashMap<String, String>();
 
-
+    //normal
     public static void SetConfig(String net, String name, String projectname, String projectnameingroupid, String mainClass) {
         Config.net = net;
         Config.name = name;
         Config.projectname = projectname;
         Config.projectnameingroupid = projectnameingroupid;
         Config.MainClass = mainClass;
-        Config.MainClassfile = getfile(Config.MainClass);
+        Config.MainClassfile = Config.MainClass + ".java";
+    }
+    //hashmap
+    public static void SetConfigHashMap(String net, String name, String projectname, String projectnameingroupid, String mainClass) {
+        map.put("net", net);
+        map.put("name", name);
+        map.put("projectname", projectname);
+        map.put("projectnameingroupid", projectnameingroupid);
+        map.put("mainClass", mainClass);
     }
 
-    public static String[] GetConfig() {
-        return new String[] {Config.net, Config.name, Config.projectname, Config.projectnameingroupid};
-    }
-    private static String getfile(String mc) {
-        return mc + ".java";
+    public static Map<String, String> getMap() {
+        return map;
     }
 }
