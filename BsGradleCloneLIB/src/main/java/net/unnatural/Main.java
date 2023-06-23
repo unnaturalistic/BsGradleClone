b67241b1d866b884d8d3199c4586afb8d19d328c
unnatural <93143883+unnaturalistic@users.noreply.github.com>
2023-06-23 22:01:29 +0330

    MainCALLasdasdads

--- BsGradleCloneLIB/src/main/java/net/unnatural/Main.java
+++ BsGradleCloneLIB/src/main/java/net/unnatural/Main.java
@@ -1,24 +1,34 @@
 package net.unnatural;
 
 import net.unnatural.Events.AvailableUpdate;
+import net.unnatural.Events.MainCall;
 
 import java.net.URL;
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public List<AvailableUpdate> updateev = new ArrayList<AvailableUpdate>();
+    public List<MainCall> MainC = new ArrayList<MainCall>();
     public static void main(String[] args) {
 
     }
-
     public void AddToUpdateEvent(AvailableUpdate AU) {
         updateev.add(AU);
     }
-
     public void UpdateIsReady(URL url, boolean emergency) {
         for (AvailableUpdate availableUpdate : this.updateev) {
             availableUpdate.AvailableUpdate(url, emergency);
         }
+    }
+    public void MainCallAdd(MainCall Mainc) {
+        MainC.add(Mainc);
+    }
+    public void MainCallEvent(String[] args) {
+        for (MainCall mainCall : this.MainC) {
+            mainCall.MainCall(args);
+        }
+
     }
 }
