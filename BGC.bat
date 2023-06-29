8b1cd1093726d23d8c70bdeda82ff4196378eb3d
orangeman12 <karos2646@gmail.com>
2023-06-29 09:33:30 -0700

    Create BGC.bat

---
+++ BGC.bat
@@ -0,0 +1,16 @@
+@echo off
+rem ----------------------------------------------
+rem Bullshit gradle clone launcher, 2023 version 1
+rem ----------------------------------------------
+set VERSION=02
+echo Checking for update...
+FOR /F "tokens=*" %%g IN ('curl https://raw.githubusercontent.com/unnaturalistic/BsGradleClone/main/version.txt') do (SET UPDATE=%%g)
+IF %VERSION%==%UPDATE% (
+echo Ver is latest
+goto MainStart
+)
+echo Update is ready
+
+:update
+curl [INSERT LINK HERE] --output BsGradleClone.jar
+:MainStart
