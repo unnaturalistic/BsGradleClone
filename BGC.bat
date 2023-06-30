6cd03f63b0a46bce282416df5382e0d1ec2c64c6
unnatural <93143883+unnaturalistic@users.noreply.github.com>
2023-06-30 17:35:13 +0330

    Update BGC.bat

--- BGC.bat
+++ BGC.bat
@@ -4,7 +4,7 @@ rem Bullshit gradle clone launcher, 2023 version 1
 rem ----------------------------------------------
 set VERSION=02
 echo Checking for update...
-FOR /F "tokens=*" %%g IN ('curl https://raw.githubusercontent.com/unnaturalistic/BsGradleClone/main/version.txt') do (SET UPDATE=%%g)
+FOR /F "tokens=*" %%g IN ('curl https://raw.githubusercontent.com/unnaturalistic/BsGradleClone/main/version.txt > nul') do (SET UPDATE=%%g)
 IF %VERSION%==%UPDATE% (
 echo Ver is latest
 goto MainStart
@@ -13,4 +13,7 @@ echo Update is ready
 
 :update
 curl [INSERT LINK HERE] --output BsGradleClone.jar
+echo Update Complete, Starting BGC
 :MainStart
+java -jar BsGradleClone.jar %1 %2 %3
+exit /b
