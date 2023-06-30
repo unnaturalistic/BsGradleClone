754bb7310fe19bde17b54a981d76e380d290764a
unnatural <93143883+unnaturalistic@users.noreply.github.com>
2023-06-30 17:30:49 +0330

    before compile

--- BsGradleCloneLIB/src/main/java/net/unnatural/Main.java
+++ BsGradleCloneLIB/src/main/java/net/unnatural/Main.java
@@ -3,6 +3,7 @@ package net.unnatural;
 import net.unnatural.Events.AvailableUpdate;
 import net.unnatural.Events.MainCall;
 import net.unnatural.Events.MsgEvent;
+import net.unnatural.Events.BeforeCompile;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -12,9 +13,11 @@ public class Main {
     public List<AvailableUpdate> updateev = new ArrayList<AvailableUpdate>();
     public List<MainCall> MainC = new ArrayList<MainCall>();
     public List<MsgEvent> MsgE = new ArrayList<MsgEvent>();
+    public List<BeforeCompile> BeforeE = new ArrayList<BeforeCompile>();
     public static void main(String[] args) {
 
     }
+
     public void AddToUpdateEvent(AvailableUpdate AU) {
         updateev.add(AU);
     }
@@ -23,6 +26,7 @@ public class Main {
             availableUpdate.AvailableUpdate(url, emergency);
         }
     }
+
     public void MainCallAdd(MainCall Mainc) {
         MainC.add(Mainc);
     }
@@ -31,6 +35,7 @@ public class Main {
             mainCall.MainCall(args);
         }
     }
+
     public void AddMsgEvent(MsgEvent ME) {
         MsgE.add(ME);
     }
@@ -38,5 +43,14 @@ public class Main {
        for (MsgEvent MsgEvent : this.MsgE) {
            MsgEvent.MsgEvent(msg);
        }
+    }
+
+    public void AddBCEvent(BeforeCompile BC) {
+        BeforeE.add(BC);
+    }
+    public void BeforeCompileEvent(String GroupID, String MainClass, String MainClassFile) {
+        for (BeforeCompile beforecompile : this.BeforeE) {
+            beforecompile.BeforeCompile(GroupID, MainClass, MainClassFile);
+        }
     }
 }
