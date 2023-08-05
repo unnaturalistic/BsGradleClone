8f0afb4b5e007d89307fda9e91e9a4ce6b4e1f9d
unnatural <93143883+unnaturalistic@users.noreply.github.com>
2023-08-05 21:44:23 +0330

    am bacj

--- src/main/java/net/unnatural/Main.java
+++ src/main/java/net/unnatural/Main.java
@@ -3,7 +3,7 @@ package net.unnatural;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URL;
+//import java.net.URL;
 import java.nio.file.Paths;
 import java.util.Map;
 
